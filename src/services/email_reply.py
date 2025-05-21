e18865eb6f3a4c402a7f51048b14be18c3095a6f
wasizafar <mdwasizafar9135@gmail.com>
2025-05-21 22:57:29 +0530

    add a function that use grop api to make reply of the mail

--- src/services/email_reply.py
+++ src/services/email_reply.py
@@ -1,15 +1,19 @@
 import google.generativeai as genai
 import os
 from dotenv import load_dotenv
+from groq import Groq
+import requests # for the use of groq API
 
 load_dotenv()  # Load API key from .env file
 
 GEMINI_API_KEY = os.getenv("GEMINI_API_KEY")
+GROK_API_KEY = os.getenv("GROK_API_KEY")
 
 # Configure the Gemini API
 genai.configure(api_key=GEMINI_API_KEY)
+client = Groq(GROK_API_KEY)
 
-def generate_reply(email_subject, email_body):
+def generate_reply_by_gemini(email_subject, email_body):
     """Generates an AI-powered reply for an email using Google's Gemini API."""
     
     prompt = f"""
@@ -25,3 +29,24 @@ def generate_reply(email_subject, email_body):
     response = model.generate_content(prompt)
 
     return response.text.strip()
+
+def generate_reply_by_groq(email_subject, email_body):
+    """Generates an AI-powered reply for an email using Google's Gemini API."""
+    # cunstruct the prompt
+    prompt = f"""You are an AI email assistant. Read the following email and generate a professional reply.
+
+    Subject: {email_subject}
+    Body: {email_body}
+
+    Your reply should be clear, concise, and professional.
+    """
+    # send request to groq chat complation API
+    chat_completion = client.chat.completions.create(
+        messages = [
+            {"role": "system", "content": "You are an AI email assistant."},
+            {"role": "user", "content": prompt}
+        ],
+        model = 'llama-3.3-70b-versatil'
+        stream = False
+    )
+    return chat_completion.choise[0].message.content.strip()
